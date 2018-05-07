a47c99ea7bd96ae8beeac76544347d7438f2360f
st-sloth <stsloth@protonmail.com>
2018-05-07 14:11:28 +0500

    init with electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'komod-accounting'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>

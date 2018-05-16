5a9e537d72929630d877d2af11dd8ceb17ffef5a
st-sloth <stsloth@protonmail.com>
2018-05-16 22:47:31 +0500

    Правки ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,22 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+  ],
   globals: {
     __static: true
   },
   plugins: [
-    'html'
+    'vue',
   ],
   'rules': {
     // allow paren-less arrow functions
@@ -21,6 +24,17 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    'comma-dangle': ['error', 'always-multiline'],
+    'vue/html-closing-bracket-newline': ['error', {
+      singleline: 'never',
+      multiline: 'always',
+    }],
+    'vue/html-closing-bracket-spacing': ['error', {
+      startTag: 'never',
+      endTag: 'never',
+      selfClosingTag: 'never',
+    }],
   }
 }
